82c2eb2e32533ce447b47f2410583ecdefdf1151
PrerakPatel13 <prerakpatel1301@gmail.com>
2024-04-16 13:20:52 +0530

    update

--- server.js
+++ server.js
@@ -7,24 +7,28 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-
+import path, { dirname } from 'path';
+import { fileURLToPath } from "url";
 dotenv.config();
 
 connectDB();
 
 const app = express();
-
+const __filename =fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-
+app.use(express.static(path.join(__dirname, './client/build')))
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
+app.use('*', function (req,res){
+  res.sendFile(path.join(__dirname,'./client/build/index.html'))
 });
+
+
 
 const PORT = process.env.PORT || 8080;
 
